94f7de974b2613b9206fcfb3736a7a7b401b5349
Irisrainbow7 <a17.bnxk@g.chuo-u.ac.jp>
2019-12-03 00:46:55 +0900

    スプライトを画像に書き出すメソッド追加

--- irisnes/cassette.py
+++ irisnes/cassette.py
@@ -1,3 +1,6 @@
+from PIL import Image
+import numpy as np
+
 class Cassette:
     def __init__(self, cassette_path):
         with open(cassette_path, 'rb') as f:
@@ -7,7 +10,7 @@ class Cassette:
         character_rom_start = 0x10 + self.cassette_data[4] * 0x4000
         character_rom_end = character_rom_start + self.cassette_data[5] * 0x2000
         self.program_rom = self.cassette_data[0x10:character_rom_start - 1]
-        self.character_rom = self.cassette_data[character_rom_start:character_rom_end - 1]
+        self.character_rom = self.cassette_data[character_rom_start:character_rom_end]
 
     def sprite(self, n):
         sprite_data = self.character_rom[n * 0x10:(n+1) * 0x10]
@@ -25,3 +28,14 @@ class Cassette:
         for i in range(len(la)):
             sprite_image_data.append(list(map(sum,zip(la[i],lb[i]))))
         return(sprite_image_data)
+
+    def sprite_to_image(self):
+        tmp = [[] for _ in range(11)]
+        for i in range(512):
+            tmp[i//50].append(self.sprite(i))
+        for i in range(11):
+            tmp[i] = np.hstack(tmp[i])
+        tmp[10] = np.hstack([tmp[10],np.zeros((8,304))])
+        image = np.vstack(tmp)
+        image *= 100
+        Image.fromarray(np.uint8(image)).save('image.png')
